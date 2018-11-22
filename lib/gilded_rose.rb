629c6e5f081b2da4885a382eaec1c6bbfa9d986d
Farah Jabri <farahjabri@hotmail.com>
2018-11-22 22:08:55 +0000

    refactored all methods, 1 test failing

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -4,55 +4,63 @@ class GildedRose
     @items = items
   end
 
+  def general(item)
+    if item.quality == 0 and item.sell_in > 0
+      item.sell_in -= 1
+    elsif item.quality > 0 and item.sell_in > 0
+      item.quality -= 1 and item.sell_in -= 1
+    elsif item.quality > 0 and item.sell_in = 0
+        item.quality -= 2
+    end
+  end
+
+  def aged_brie(item)
+    if item.quality == 50 and item.sell_in > 0
+      item.sell_in -= 1
+    elsif item.quality == 49 and item.sell_in > 0
+      item.quality += 1 and item.sell_in -= 1
+    elsif item.quality < 49 and item.sell_in == 0
+      item.quality += 2
+    else item.quality < 50 and item.sell_in == 0
+      item.quality += 1
+    end
+  end
+
+  def sulfuras(item)
+    item.quality = item.quality and item.sell_in = item.sell_in
+  end
+
+  def backstage_pass(item)
+    if item.sell_in == 0
+      item.quality = 0
+    elsif item.quality == 50 and item.sell_in > 0
+      item.sell_in -= 1
+    elsif item.quality < 50 and item.sell_in > 10
+      item.quality += 1 and item.sell_in -= 1
+    elsif item.quality < 48 and item.sell_in < 6
+      item.quality += 3 and item.sell_in -= 1
+    else item.quality < 50 and item.sell_in < 11
+      item.quality += 2 and item.sell_in -= 1
+    end
+  end
+
   def update_quality
     brie = "Aged Brie"
     backstage = "Backstage passes to a TAFKAL80ETC concert"
     sulfuras = "Sulfuras, Hand of Ragnaros"
 
     @items.each do |item|
-      # step 1
-      if item.name != brie and item.name != backstage
-        if item.quality > 0 and item.name != sulfuras
-            item.quality = item.quality - 1
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          # step 2
-          if item.name == backstage
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != sulfuras
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != brie
-          if item.name != backstage
-            if item.quality > 0
-              if item.name != sulfuras
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if item.name != brie and item.name != backstage and item.name != sulfuras
+        general(item)
+      elsif item.name == brie
+        aged_brie(item)
+      elsif item.name == backstage
+        backstage_pass(item)
+      else item.name == sulfuras
+        sulfuras(item)
       end
     end
   end
+
+
 end
--- spec/farah_guilded_rose_spec.rb
+++ spec/farah_guilded_rose_spec.rb
@@ -101,9 +101,9 @@ describe GildedRose do
       expect(item.quality).to eq(50)
     end
   end
-
-  # * "Sulfuras", being a legendary item, never has to be sold or decreases in
-  #   `quality`
+  #
+  # # * "Sulfuras", being a legendary item, never has to be sold or decreases in
+  # #   `quality`
 
   describe "Sulfuras" do
     it "never decreses in quality" do
@@ -119,13 +119,13 @@ describe GildedRose do
     end
   end
 
-  # * "Backstage passes", like aged brie, increases in `quality` as its `sell_in` value
-  #   approaches; `quality` increases by 2 when there are 10 days or less and by 3
-  #   when there are 5 days or less but `quality` drops to 0 after the concert
-
+  # # * "Backstage passes", like aged brie, increases in `quality` as its `sell_in` value
+  # #   approaches; `quality` increases by 2 when there are 10 days or less and by 3
+  # #   when there are 5 days or less but `quality` drops to 0 after the concert
+  #
   describe "Backstage passes" do
 
-    it "increases in quality by 1" do
+    it "increases in quality by 1 if sell_in is greater than 10" do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 25)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(26)
@@ -137,13 +137,13 @@ describe GildedRose do
       expect(item.quality).to eq(27)
     end
 
-    it "increases three times in quality if the sell_in in 10 or less" do
+    it "increases three times in quality if the sell_in in 5 or less" do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 25)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(28)
     end
 
-    it "increases three times in quality if the sell_in in 10 or less" do
+    it "sets the quality to zero if sell_in is zero" do
       item = Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 25)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(0)
@@ -155,15 +155,20 @@ describe GildedRose do
       expect(item.quality).to eq(50)
     end
 
-  end
-
-  # * "Conjured" items degrade in `quality` twice as fast as normal items
-
-  describe "Conjured items - no idea what they are..." do
-    xit "degrades twice as fast as general items" do
-      item = Item.new("Conjured", 4, 25)
+    it 'only raises quality to 50 when quality is at 49' do
+      item = Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 49)
       GildedRose.new([item]).update_quality
-      expect(item.quality).to eq(23)
+      expect(item.quality).to eq(50)
     end
   end
+  #
+  # # * "Conjured" items degrade in `quality` twice as fast as normal items
+  #
+  # describe "Conjured items - no idea what they are..." do
+  #   xit "degrades twice as fast as general items" do
+  #     item = Item.new("Conjured", 4, 25)
+  #     GildedRose.new([item]).update_quality
+  #     expect(item.quality).to eq(23)
+  #   end
+  # end
 end
