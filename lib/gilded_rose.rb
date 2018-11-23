3291be6caf4af0c4858c421c63c3dd0e3a602f2d
Farah Jabri <farahjabri@hotmail.com>
2018-11-23 10:21:01 +0000

    passing tests

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -35,19 +35,24 @@ class GildedRose
       item.quality = 0
     elsif item.quality == 50 and item.sell_in > 0
       item.sell_in -= 1
-    elsif item.quality < 50 and item.sell_in > 10
-      item.quality += 1 and item.sell_in -= 1
-    elsif item.quality < 48 and item.sell_in < 6
+    elsif item.quality <= 47 and item.sell_in <= 5
       item.quality += 3 and item.sell_in -= 1
-    else item.quality < 50 and item.sell_in < 11
+    elsif item.quality <= 48 and item.sell_in <= 10
       item.quality += 2 and item.sell_in -= 1
+    elsif item.quality < 50 and item.sell_in >= 10
+      item.quality += 1 and item.sell_in -= 1
     end
+  end
+
+  def conjured(item)
+    
   end
 
   def update_quality
     brie = "Aged Brie"
     backstage = "Backstage passes to a TAFKAL80ETC concert"
     sulfuras = "Sulfuras, Hand of Ragnaros"
+    conjured = "Conjured"
 
     @items.each do |item|
       if item.name != brie and item.name != backstage and item.name != sulfuras
