17764fa987aade7bd0cd5b5c3915788cb5d99066
Farah Jabri <farahjabri@hotmail.com>
2018-11-27 14:38:52 +0000

    refactored the if statement

--- lib/generalitems.rb
+++ lib/generalitems.rb
@@ -1,11 +1,11 @@
 class GeneralItems
-
   def general(item)
-    if item.quality > 0 and item.sell_in > 0
-      item.quality -= 1 and item.sell_in -= 1
-    elsif item.quality > 0 and item.sell_in = 0
-      item.quality -= 2
+    if item.quality > 0
+      if item.sell_in > 0
+        item.quality -= 1 and item.sell_in -= 1
+      elsif item.sell_in = 0
+        item.quality -= 2
+      end
     end
   end
-
 end
