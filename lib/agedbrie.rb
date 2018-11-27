f60be40cd1f008823b191f4318b9f4742f98dd14
Farah Jabri <farahjabri@hotmail.com>
2018-11-27 15:06:22 +0000

    refactored if statement

--- lib/agedbrie.rb
+++ lib/agedbrie.rb
@@ -1,11 +1,11 @@
 class AgedBrie
-
   def aged_brie(item)
-    if item.quality == 50 and item.sell_in > 0
-      item.quality = 50
-    elsif item.quality < 50 and item.sell_in > 0
-      item.quality += 1 and item.sell_in -= 1
+    if item.sell_in > 0
+      if item.quality == 50
+        item.quality = 50
+      elsif item.quality < 50
+        item.quality += 1 and item.sell_in -= 1
+      end
     end
   end
-
 end
