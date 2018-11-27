b22b32680dd4a3beabfd946ed5d433adcc5b9eec
Farah Jabri <farahjabri@hotmail.com>
2018-11-27 15:06:13 +0000

    refactored if statement

--- lib/backstagepass.rb
+++ lib/backstagepass.rb
@@ -3,14 +3,18 @@ class BackStagePass
   def backstage_pass(item)
     if item.sell_in == 0
       item.quality = 0
-    elsif item.quality == 50 and item.sell_in > 0
-      item.sell_in -= 1
-    elsif item.quality < 50 and item.sell_in > 10
-      item.quality += 1 and item.sell_in -= 1
+      if item.quality == 50
+        item.sell_in -= 1
+        if item.sell_in > 0
+          item.sell_in -= 1
+        end
+      end
     elsif item.quality <= 47 and item.sell_in <= 5
-      item.quality += 3 and item.sell_in -= 1
+      item.quality += 3
     elsif item.quality <= 48 and item.sell_in <= 10
-      item.quality += 2 and item.sell_in -= 1
+      item.quality += 2
+    elsif item.quality < 50 and item.sell_in > 9
+      item.quality += 1
     end
   end
 
