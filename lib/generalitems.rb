9118cd727d920abce53754403f3c9167fe6e5f97
Farah Jabri <farahjabri@hotmail.com>
2018-12-15 21:51:35 +0000

    refactored general items tests

--- lib/generalitems.rb
+++ lib/generalitems.rb
@@ -7,25 +7,4 @@ class GeneralItems < Item
     @sell_in -= 1 if @sell_in > 0
     @quality -= 2 if @sell_in.zero?
   end
-
-
-  # def general(item)
-  #   if item.quality > 0
-  #     if item.sell_in > 0
-  #       reduce_both_by_one(item)
-  #     elsif item.sell_in.zero?
-  #       item.quality -= 2
-  #     end
-  #   else
-  #     error_logger
-  #   end
-  # end
-  #
-  # def reduce_both_by_one(item)
-  #   item.quality -= 1 and item.sell_in -= 1
-  # end
-  #
-  # def error_logger
-  #   return "There seems to be a problem, please try again"
-  # end
 end
