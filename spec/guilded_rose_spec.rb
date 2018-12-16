67d0d84ba4bdec212534867c03bfdbfcb0989722
Farah Jabri <farahjabri@hotmail.com>
2018-12-16 01:51:42 +0000

    gildedrose specs

--- spec/guilded_rose_spec.rb
+++ spec/guilded_rose_spec.rb
@@ -1,10 +1,31 @@
 require 'gilded_rose'
+require 'backstagepass'
 require 'generalitems'
+require 'conjured'
 require 'item'
 
 describe GildedRose do
-  # # * "Backstage passes", like aged brie, increases in `quality` as its `sell_in` value
-  # #   approaches; `quality` increases by 2 when there are 10 days or less and by 3
-  # #   when there are 5 days or less but `quality` drops to 0 after the concert
+  it "calles the update_quality method on an array of items #BackStagePass" do
+    item1 = BackStagePass.new("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+    item2 = Conjured.new("Conjured", 4, 25)
+    item3 = GeneralItems.new("Grapes", 5, 10)
+    GildedRose.new([item1, item2, item3]).update_quality
+    expect(item1.quality).to eq(50)
+  end
 
+  it "calles the update_quality method on an array of items #Conjured" do
+    item1 = BackStagePass.new("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+    item2 = Conjured.new("Conjured", 4, 25)
+    item3 = GeneralItems.new("Grapes", 5, 10)
+    GildedRose.new([item1, item2, item3]).update_quality
+    expect(item2.quality).to eq(23)
+  end
+
+  it "calles the update_quality method on an array of items #GeneralItems" do
+    item1 = BackStagePass.new("Backstage passes to a TAFKAL80ETC concert", 3, 50)
+    item2 = Conjured.new("Conjured", 4, 25)
+    item3 = GeneralItems.new("Grapes", 5, 10)
+    GildedRose.new([item1, item2, item3]).update_quality
+    expect(item3.quality).to eq(9)
+  end
 end
