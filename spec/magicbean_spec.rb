b38250cbad0de8b2767f28a15779aa2e3314f46f
Farah Jabri <farahjabri@hotmail.com>
2018-12-16 12:14:15 +0000

    updated stub

--- spec/magicbean_spec.rb
+++ spec/magicbean_spec.rb
@@ -5,21 +5,21 @@ describe MagicBean do
   describe "magicbean quality" do
     it "it randomly decreases in quality by 1" do
       item = MagicBean.new("magicBean", 4, 5)
-      item.stub(:randomly).and_return(1)
+      allow(item).to receive(:randomly).and_return(1)
       item.update_item
       expect(item.quality).to eq(4)
     end
 
     it "it randomly decreases in quality by 0" do
       item = MagicBean.new("magicBean", 4, 5)
-      item.stub(:randomly).and_return(0)
+      allow(item).to receive(:randomly).and_return(1)
       item.update_item
       expect(item.quality).to eq(5)
     end
 
     it "is never less than zero" do
       item = MagicBean.new("magicBean", 4, 1)
-      item.stub(:randomly).and_return(1)
+      allow(item).to receive(:randomly).and_return(1)
       item.update_item
       expect(item.quality).to eq(0)
       item.update_item
