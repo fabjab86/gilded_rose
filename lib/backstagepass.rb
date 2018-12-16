432bdd3384f5f01dec81f80baea5810374d1d5f9
Farah Jabri <farahjabri@hotmail.com>
2018-12-16 01:36:39 +0000

    refactored one functionality

--- lib/backstagepass.rb
+++ lib/backstagepass.rb
@@ -4,23 +4,14 @@ class BackStagePass < Item
 
   def update_item
     @quality = 0 if @sell_in.zero?
-    if quality_is_fifty
-      sell_in_minus_one
-    elsif sell_in_less_5
+    @sell_in -= 1 if @quality == 50
+    if sell_in_less_5
       quality_plus_three
     elsif sell_in_less_10
       quality_plus_two
     elsif quality_less_50
       quality_plus_one
     end
-  end
-
-  def quality_is_fifty
-    @quality == 50
-  end
-
-  def sell_in_minus_one
-    @sell_in -= 1
   end
 
   def sell_in_less_5
