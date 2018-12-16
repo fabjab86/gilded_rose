471eb93abdc0912d8e1557ebac9d7b6add5d993f
Farah Jabri <farahjabri@hotmail.com>
2018-12-16 13:30:42 +0000

    refectored backstage

--- lib/backstagepass.rb
+++ lib/backstagepass.rb
@@ -1,41 +1,18 @@
 require_relative 'item'
 
 class BackStagePass < Item
-
+  
   def update_item
-    @quality = 0 if @sell_in.zero?
     @sell_in -= 1 if @quality == 50
-    if sell_in_less_5
-      quality_plus_three
-    elsif sell_in_less_10
-      quality_plus_two
-    elsif quality_less_50
-      quality_plus_one
-    end
+    @quality += 1 and @sell_in -= 1 if @quality == 49
+    increase_quality if quality > 0 and sell_in > 1
+    @quality = 0 if sell_in.zero?
   end
 
-  def sell_in_less_5
-    @quality <= 47 and @sell_in <= 5 and @sell_in != 0
-  end
-
-  def quality_plus_three
-    @quality += 3 and @sell_in -= 1
-  end
-
-  def sell_in_less_10
-    @quality <= 48 and @sell_in <= 10 and @sell_in != 0
-  end
-
-  def quality_plus_two
-    @quality += 2 and @sell_in -= 1
-  end
-
-  def quality_less_50
-    @quality < 50 and @sell_in > 4 and @sell_in != 0
-  end
-
-  def quality_plus_one
-    @quality += 1 and @sell_in -= 1
+  def increase_quality
+    @quality += 1 and @sell_in -= 1 if sell_in > 10 and quality < 50
+    @quality += 2 and @sell_in -= 1 if sell_in <= 10 and quality <= 48
+    @quality += 1 and @sell_in -= 1 if sell_in <= 5 and quality < 50
   end
 
 end
