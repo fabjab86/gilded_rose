4806c2a19931589c89a8b08b46a90e4b8b1b9704
Farah Jabri <farahjabri@hotmail.com>
2018-12-18 18:35:22 +0000

    final refactor for backstagepass

--- lib/backstagepass.rb
+++ lib/backstagepass.rb
@@ -3,32 +3,27 @@ require_relative 'item'
 class BackStagePass < Item
 
   def update_item
-    @sell_in -= 1 if @quality == 50
-    increase_by_one_if_quality_is_49
-    increase_quality if sell_in > 1 and quality > 0
-    @quality = 0 if sell_in.zero?
+    if sell_in.zero?
+      @quality = 0
+    elsif @quality == 50
+      @sell_in -= 1
+    elsif @quality < 50 and @quality != 0
+      increase_by_one
+      increase_by_two
+      increase_by_three
+    end
   end
 
-  def increase_quality
-    if_more_than_10_sell_in_days
-    if_less_than_11_sell_in_days
-    if_less_than_6_sell_in_days
+  def increase_by_one
+    @quality += 1 and @sell_in -= 1 if @quality != 50
   end
 
-  def increase_by_one_if_quality_is_49
-    @quality += 1 and @sell_in -= 1 if @quality == 49
+  def increase_by_two
+    @quality += 1 and @sell_in -= 1 if @sell_in < 11 and @quality != 50
   end
 
-  def if_more_than_10_sell_in_days
-    @quality += 1 and @sell_in -= 1 if sell_in > 10 and quality < 50
-  end
-
-  def if_less_than_11_sell_in_days
-    @quality += 2 and @sell_in -= 1 if sell_in <= 10 and quality <= 48
-  end
-
-  def if_less_than_6_sell_in_days
-    @quality += 1 and @sell_in -= 1 if sell_in <= 5 and quality < 50
+  def increase_by_three
+    @quality += 1 and @sell_in -= 1 if @sell_in < 6 and @quality != 50
   end
 
 end
